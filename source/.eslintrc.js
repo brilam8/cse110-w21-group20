062f10e212b8f4558070e600c7a639d3e79a046f
tommytran8 <trantommy71@gmail.com>
2021-02-04 22:21:13 -0800

    added Jest, ESLint, Html Linting

--- source/.eslintrc.js
+++ source/.eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true,
-        "node": true
-    
+        "commonjs": true,
+        "es2021": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+        "ecmaVersion": 12
     },
     "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
     }
 };
