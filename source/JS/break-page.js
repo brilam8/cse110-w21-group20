562711739c753f9408b48791a1859efdf5cd061f
ZongchenLi <zoli@ucsd.edu>
2021-02-17 03:28:31 -0800

    fix one small bug

--- source/JS/break-page.js
+++ source/JS/break-page.js
@@ -1,17 +1,16 @@
-
-const startMinute= 5;
+const startMinute = 5;
 let time = startMinute *60;
-
 const countdown = document.getElementById('break-timer');
-// const button = document.getElementById('break-button');
+const button = document.getElementById('break-button');
+
 
 setInterval(updateCountdown,1000);
 
 
 function updateCountdown(){
     const minute = Math.floor(time/60);
-    let second = time% 60;
-    second = second =0 ? abortTimer(): abortTimer()
+    let second = time % 60;
+    second = second == 0 ? abortTimer(): abortTimer()
     second = second < 5 ? '0' + second: second;
     countdown.innerHTML= `${minute}: ${second}`;
     time --;
@@ -21,17 +20,12 @@ function updateCountdown(){
 function abortTimer(){
     clearInterval(state);
     reset_timer();
-    setTimerString();
 }
 
-
 function reset_timer(){
-    state = null;
-    counter = time;
     button.innerHTML = "Aborted";
 } 
 
-
 button.addEventListener('click', ()=>{
-     abortTimer();
+    abortTimer();
 });
