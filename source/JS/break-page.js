698b29d5b5cc9854f1d55f08595861dc65505b33
ZongchenLi <zoli@ucsd.edu>
2021-02-17 00:16:28 -0800

    Set up the break-page and add functionality of timing

--- source/JS/break-page.js
+++ source/JS/break-page.js
@@ -1,1 +1,18 @@
+const startMinute= 5;
+let time = startMinute *60;
 
+const countdown = document.getElementById('break-timer');
+
+
+setInterval(updateCountdown,1000);
+
+function updateCountdown(){
+
+    const minute = Math.floor(time/60);
+    let second = time% 60;
+
+    second = second <5? '0'+second:second;
+    countdown.innerHTML= `${minute}:${second}`;
+    time --;
+    
+}
