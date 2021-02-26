52c40a1d5c0635deaf456c77cc26ab11f14849b0
tommytran8 <trantommy71@gmail.com>
2021-02-25 22:17:52 -0800

    try to fix cypress

--- source/cypress/plugins/index.js
+++ source/cypress/plugins/index.js
@@ -12,6 +12,8 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+/* eslint-disable */
+
 /**
  * @type {Cypress.PluginConfig}
  */
@@ -19,3 +21,5 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+
+/* eslint-enable */
