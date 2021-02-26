d3f4472a84c1c8af22410e98bad6a245ae2e3449
Megan <mlc025@ucsd.edu>
2021-02-25 17:23:10 -0800

    update test for ocmbined setup page

--- source/TESTS/how-to-page.test.js
+++ source/TESTS/how-to-page.test.js
@@ -14,13 +14,13 @@ describe ('redirect buttons', () => {
   test('redirects to set-up page', () => {
     document.body.innerHTML =
     `
-    <a href="set-up-page.html" id="to-set-up-page">
+    <a href="setup-active-break-pages.html" id="to-set-up-page">
       <button class="wb-button">Set Up</button>
     </a>
 
     `
     const setupButton = document.getElementById("to-set-up-page");
-    expect(setupButton.getAttribute('href')).toEqual('set-up-page.html');
+    expect(setupButton.getAttribute('href')).toEqual('setup-active-break-pages.html');
   });
 });
 
