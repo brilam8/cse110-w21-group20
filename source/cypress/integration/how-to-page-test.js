6280cb1e29c63f627ec2a461efa49d2be690b1b3
Megan <mlc025@ucsd.edu>
2021-03-09 14:39:44 -0800

    fix cypress tests

--- source/cypress/integration/how-to-page-test.js
+++ source/cypress/integration/how-to-page-test.js
@@ -1,18 +1,19 @@
 describe('how to page Tests', () => {
     beforeEach(() => {
-        cy.visit('http://127.0.0.1:5500/HTML/how-to-page.html');
+        cy.visit('http://127.0.0.1:5500/source/HTML/how-to-page.html');
     });
     it("sample test", ()=>{
         expect(true).to.equal(true);
     });
 
     it('Home page button redirects to landing page', () => {
-        cy.get('to-home-page').click();
-        cy.url().should('eq', 'http://127.0.0.1:5500/HTML/landing-page.html');
+        cy.get('#to-home-page').click();
+        cy.url().should('eq', 'http://127.0.0.1:5500/source/HTML/landing-page.html');
     });
 
     it('Set-up page button redirects to set-up page', () => {
-        cy.get('to-set-up-page').click();
-        cy.url().should('eq', 'http://127.0.0.1:5500/HTML/setup-active-break-pages.html');
+        cy.get('#to-set-up-page').click();
+        cy.url().should('eq', 'http://127.0.0.1:5500/source/HTML/setup-active-break-pages.html');
     });
 });
+
