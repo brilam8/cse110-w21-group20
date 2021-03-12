de61ff78ebf6a4cb3794a42712c29296c09b47a9
tommytran8 <trantommy71@gmail.com>
2021-03-11 21:17:24 -0800

    retry on ubuntu

---
+++ source/app.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 700,
+    minHeight: 500,
+    icon: __dirname + './assets/logo.ico'
+  })
+
+  win.loadFile('./HTML/landing-page.html')
+  win.setMenuBarVisibility(false)
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
